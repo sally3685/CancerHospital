63fe84e3a63e0d44305d842b51070c64eacc575b
Sally Ismail <he84tt@gmail.com>
2025-02-11 19:26:36 +0300

    c

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: { esmExternals: true },
 };
 
 export default nextConfig;
